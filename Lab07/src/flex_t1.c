6007aad4363feae0721a8614e02327a4e02057f6
Keith Skinner <brian.skinner121@myci.csuci.edu>
2018-10-19 00:18:14 -0700

    super easy lab this week. DONE

--- Lab07/src/flex_t1.c
+++ Lab07/src/flex_t1.c
@@ -1,11 +1,34 @@
+/**
+* Name: Keith Skinner
+* Lab: Lab 7 Introduction to flex
+* Date: 10/23/2018
+**/
+
 #include "flex_t1.h"
 
 int main(void) {
 
-   TOKEN tok;
+    TOKEN tok;
 
-   char *token[] = {"if", "then" /* and so on */};
+    char *token[] = {
+            "if", "then", "while", "do", "print",
+            ";", "=", "(", ")", "+", "-", "*", "/", "%"
+    };
 
-   while ((tok = yylex()) != 0)
-      printf("{<keyword> \"%s\"}", token[tok - IF_TOKEN]);
+    char *type[] = {
+            "keyword", "keyword", "keyword", "keyword", "keyword",
+            "semicolon", "assignment", "lparen", "rparen",
+            "plus", "minus", "multiply", "divide", "modulus"
+    };
+
+    freopen("../flex_t1in.txt", "r", stdin);
+
+    while ((tok = yylex()) != 0) {
+        if (tok == ID_TOKEN)
+            printf("{<identifier> \"%s\"}", yytext);
+        else if (tok == NUMBER_TOKEN)
+            printf("{<number> \"%s\"}", yytext);
+        else
+            printf("{<%s> \"%s\"}", type[tok - IF_TOKEN], token[tok - IF_TOKEN]);
+    }
 }
