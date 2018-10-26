a92c52b8aff398cf4437243c198284e258439427
Keith Skinner <brian.skinner121@myci.csuci.edu>
2018-10-25 20:31:55 -0700

    done. kill me. i hate yacc

--- Lab08/Question2/src/ciProlog.c
+++ Lab08/Question2/src/ciProlog.c
@@ -24,39 +24,62 @@ void yyerror(char *s)
 
 bool not(bool logexpr1)
 {
-    // TODO implement the function
-
-    return false;
+    // TODO implement the function - DONE
+    return !logexpr1;
 }
 
 bool and(bool logexpr1, bool logexpr2)
 {
-    // TODO implement the function
-
-    return false;
+    // TODO implement the function - DONE
+    return logexpr1 && logexpr2;
 }
 
 bool or(bool logexpr1, bool logexpr2)
 {
-    // TODO implement the function
-
-    return false;
+    // TODO implement the function - DONE
+    return logexpr1 || logexpr2;
 }
 
 // add the assertion to the database
 // finding a proposition in the list makes it true
 bool assert(char *proposition)
 {
-    // TODO implement the function
-    return false;
+    // TODO implement the function - DONE
+    if (!eval(proposition)) {
+        PROP_TYPE *temp = malloc(sizeof(PROP_TYPE));
+        temp->name = malloc(strlen(proposition)+1);
+        strcpy(temp->name, proposition);
+        temp->next = NULL;
+        kb_tail->next = temp;
+        kb_tail = temp;
+    }
+    return true;
 }
 
 // remove the assertion to the database
 // after that the proposition is assumed to be false -- it cannot be found
 bool retract(char *proposition)
 {
-    // TODO implement the function
-    return false;
+    // TODO implement the function - DONE
+    bool found = false;
+    PROP_TYPE ** iter = &kb_head;
+    while (!found && (*iter)->next != NULL) {
+        //found proposition
+        if (strcmp(proposition, (*iter)->next->name) == 0) {
+            found = true;
+            //if tail, move tail back one
+            if ((*iter)->next == kb_tail)
+                kb_tail = (*iter);
+            //move next ptr to skip found node
+            PROP_TYPE * temp = (*iter)->next;
+            (*iter) = (*iter)->next->next;
+            //delete found node
+            free(temp->name);
+            free(temp);
+        }
+        else iter = &((*iter)->next);
+    }
+    return found;
 }
 
 // find the proposition in the knowledge base
@@ -64,6 +87,13 @@ bool retract(char *proposition)
 // otherwise, return false
 bool eval(char *proposition)
 {
-    // TODO implement the function
-    return false;
+    // TODO implement the function - DONE
+    bool found = false;
+    PROP_TYPE * iter = kb_head;
+    while (!found && iter != NULL) {
+        if (strcmp(proposition, iter->name) == 0)
+            found = true;
+        else iter = iter->next;
+    }
+    return found;
 };
--- Lab08/Question2/src/ciProlog.y
+++ Lab08/Question2/src/ciProlog.y
@@ -1,18 +1,16 @@
 /*
+logexpr ::=
+proposition
+| assert ( proposition )
+| retract ( proposition )
+| ( logexpr )
+| NOT logexpr
+| logexpr AND logexpr
+| logexpr OR logexpr
 
- logexpr ::= 
-   proposition
- | assert ( proposition )
- | retract ( proposition )
- | ( logexpr )
- | NOT logexpr
- | logexpr AND logexpr
- | logexpr OR logexpr
-
- letter ::= [a-zA-Z]
- proposition ::= '{letter}({letter}|\ )*'
-
- */
+letter ::= [a-zA-Z]
+proposition ::= '{letter}({letter}|\ )*'
+*/
 
 %{
 #include "ciProlog.h"
@@ -31,19 +29,47 @@
 
 %%
 
-program: logexpr EOL {
+program:
+    logexpr EOL {
         fprintf(stderr, "yacc: program ::= logexpr EOL\n");
         printf("=> %s\n", bool2str($1));
-}
-;
+    };
 
 logexpr:
-HALT {
-   fprintf(stderr, "yacc: logexpr ::= HALT\n");
-   exit(0);
-}
-/* TODO complete the definition of the grammar */
-;
+    PROPOSITION {
+        fprintf(stderr, "yac: PROPOSITION %s\n", $1);
+        $$ = eval($1);
+    }|
+    ASSERT LPAREN PROPOSITION RPAREN {
+        fprintf(stderr, "yac: ASSERT ( %s )\n", $3);
+        $$ = assert($3);
+    }|
+    RETRACT LPAREN PROPOSITION RPAREN {
+        fprintf(stderr, "yac: RETRACT ( %s )\n", $3);
+        $$ = retract($3);
+    }|
+    LPAREN logexpr RPAREN {
+        fprintf(stderr, "yac: ( %s )\n", bool2str($2));
+        $$ = $2;
+    }|
+    NOT logexpr {
+        fprintf(stderr, "yac: NOT %s\n", bool2str($2));
+        $$ = not($2);
+    }|
+    logexpr AND logexpr {
+        fprintf(stderr, "yac: %s AND %s\n", bool2str($1), bool2str($3));
+        $$ = and($1, $3);
+    }|
+    logexpr OR logexpr {
+        fprintf(stderr, "yac: %s OR %s\n", bool2str($1), bool2str($3));
+        $$ = or($1, $3);
+    }|
+    HALT {
+        fprintf(stderr, "yacc: logexpr ::= HALT\n");
+        exit(0);
+    }
+    /* TODO complete the definition of the grammar */
+    ;
 
 %%
 
